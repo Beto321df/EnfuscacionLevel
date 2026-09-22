3b4420c1a713192b678b4a5f36fd7e45407f5d30
Beto321df <h4ck666q@gmail.com>
2026-09-22 04:41:45 -0500

    Enable compact registerizer trace

--- tests/debug-opt.js
+++ tests/debug-opt.js
@@ -57,6 +57,7 @@ for k,v in next,t do print(k,v) end`;
 try {
   const native=buildNativeProgram(genericSource,{fallback:false,polymorphOptions:{chance:0,maxPerFunction:0}});
   console.log('NATIVE',JSON.stringify(native.functions[0].code.slice(0,16)));
+  native.functions[0].__debugCompact=true;
   const reg=registerizeProgram(JSON.parse(JSON.stringify(native)));
   validateRegisterProgram(reg);
   const output=[];

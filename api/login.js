87dafbe1c818974200e084b0f1f3bf882d62210b
Beto321df <h4ck666q@gmail.com>
2026-09-21 19:50:19 -0500

    fix: make login endpoint CORS and preflight safe

--- api/login.js
+++ api/login.js
@@ -1,6 +1,13 @@
 module.exports = async function handler(req, res) {
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
     res.setHeader('Cache-Control', 'no-store');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
+
+    if (req.method === 'OPTIONS') {
+        return res.status(204).end();
+    }
 
     if (req.method !== 'POST') {
         return res.status(405).json({
